875e59baa9a16ddc0613ff5b3ad260b0bee9f689
Austin Wonka <wonkaaustin@gmail.com>
2022-01-17 16:26:48 -0500

    committing starter code for serverjs

---
+++ server.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
