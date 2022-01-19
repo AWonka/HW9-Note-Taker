5aa8b55a55623447c1938fc075ef0b08dbc2a95d
Austin Wonka <wonkaaustin@gmail.com>
2022-01-19 16:57:39 -0500

    after hours of work I found out we had starter code...replacing all with starter code

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const db = require('./db/db.json');
+const api = require('./routes/index');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,3 +9,16 @@ app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', api);
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.listen(PORT, function() {
+        console.log(`App listening at http://localhost:${PORT}`);
+});
